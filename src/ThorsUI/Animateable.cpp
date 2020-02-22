2b040e9edf3acaa3f57f0a78846a64aaeee2323f
Loki <Loki.Astari@gmail.com>
2020-02-21 21:22:15 -0800

    Make the animation more generic.
        The animation loop does not "reset()" the state of the dc automatically.
        This is to allow multiple object animation to be painted onto the dc independently.
    
        This causes some issues around counting the animation frame, that have some
        convoluted tests to make sure we go from [0 .. max)

--- src/ThorsUI/Animateable.cpp
+++ src/ThorsUI/Animateable.cpp
@@ -2,15 +2,35 @@
 
 using namespace ThorsAnvil::ThorsUI;
 
-void Animateable::drawAnimation(wxDC& dc) const
+/*
+ * The generic use case for animation involves the calling of
+ *   1. animateOneStep()        followed by a call to
+ *   2. Refresh()               this will result in a call to
+ *   3. draw()                  which for Animatable objects (see draw function here) calls
+ *   4. drawAnimation()
+ *
+ * drawAnimation() uses `animateStep - 1` as the previous call to animateOneStep()
+ * is done the animation for step `animateStep` but then incremented the variable `animateStep`
+ * so have worked out the details for the previous step.
+ *
+ * If you examine the function animationStep() you will see that we allow incrementing of
+ * `animateStep` up to the max value but make sure it resets to zero before actual use.
+ * This means the animation step range is [0 .. max) (note max not includes). When called via
+ * animationStep() we pass the current value as that is the value we are generating the
+ * animation for while draw() uses the value `animateStep - 1` as we want to draw the value
+ * we have already done the calculations for.
+ *
+ * I hope that was clear?
+ */
+void Animateable::draw(wxDC& dc) const
 {
-    draw(dc);
+    drawAnimation(dc, animateStep - 1);
 }
 
 void Animateable::animateOneStep()
 {
     std::unique_ptr<wxDC> dc = animationDC();
-    if (animateStep == 0)
+    if (animateStep == 0 || animateStep >= animationMaxStep())
     {
         animateReset(*dc);
     }
@@ -25,17 +45,42 @@ void Animateable::animateReset(wxDC& dc)
 
 void Animateable::animationStep(wxDC& dc)
 {
-    animationStepDo(dc, animateStep);
     int maxStep = animationMaxStep();
-    animateStep = maxStep == 0 ? 0 : (animateStep + 1) % maxStep;
+    if (animateStep >= maxStep)
+    {
+        animateStep = 0;
+    }
+    animationStepDo(dc, animateStep);
+    ++animateStep;
 }
 
-void Animateable::animateOneLoop(wxDC& dc)
+void Animateable::animateOneLoop(wxDC& dc, bool reset)
 {
+    int maxStep = animationMaxStep();
     animateStep = 0;
+    if (reset)
+    {
+        animateReset(dc);
+    }
     do
     {
         animationStep(dc);
     }
-    while (animateStep != 0);
+    while (animateStep < maxStep);
+}
+
+std::unique_ptr<wxDC> Animateable::animationDC()
+{
+    return std::unique_ptr<wxDC>(nullptr);
+}
+
+void Animateable::animateResetDo(wxDC& /*dc*/)
+{}
+
+void Animateable::animationStepDo(wxDC& /*dc*/, int /*step*/)
+{}
+
+int Animateable::animationMaxStep() const
+{
+    return 0;
 }
--- src/ThorsUI/Animateable.h
+++ src/ThorsUI/Animateable.h
@@ -16,17 +16,18 @@ class Animateable: public Drawable
         Animateable()
             : animateStep(0)
         {}
-        virtual void    drawAnimation(wxDC& dc) const;
+        virtual void    draw(wxDC& dc) const;
+        virtual void    drawAnimation(wxDC& dc, int step) const       = 0;
 
                 void    animateOneStep();
                 void    animateReset(wxDC& dc);
                 void    animationStep(wxDC& dc);
-                void    animateOneLoop(wxDC& dc);
+                void    animateOneLoop(wxDC& dc, bool reset = true);
 
-        virtual std::unique_ptr<wxDC>   animationDC()       = 0;
-        virtual void    animateResetDo(wxDC& dc)            = 0;
-        virtual void    animationStepDo(wxDC& dc, int step) = 0;
-        virtual int     animationMaxStep() const            = 0;
+        virtual std::unique_ptr<wxDC>   animationDC();
+        virtual void    animateResetDo(wxDC& dc);
+        virtual void    animationStepDo(wxDC& dc, int step);
+        virtual int     animationMaxStep() const;
 };
 
     }
